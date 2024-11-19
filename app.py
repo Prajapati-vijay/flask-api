e1adf8e3fcc43beed4fe44290499c23d6a915267
Vijay <vijay@finflock.com>
2024-11-19 15:00:50 +0530

    requirements added

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text  #
+from sqlalchemy import text
+from flasgger import Swagger
+from flask import redirect
 
 app = Flask(__name__)
 
 # Replace with your SQL Server connection details
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SA:vijay123@doker.host.local/student?driver=ODBC+Driver+17+for+SQL+Server'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SA:vijay123@FINFLOCK2\\SQLEXPRESS/student?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SA:vijay123@host.docker.internal/student?driver=ODBC+Driver+17+for+SQL+Server'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Set Swagger UI URL to root `/`
+app.config['SWAGGER'] = {
+    'uiversion': 3,
+    'openapi': '3.0.2'
+}
+swagger = Swagger(app)
+
+@app.route('/')
+def index():
+    return redirect('/apidocs')
+
+
 @app.route('/students', methods=['GET'])
 def get_students():
-    # Use the text function to wrap the raw SQL query
+    """
+    Get all students
+    ---
+    responses:
+      200:
+        description: A list of students
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+                description: The student ID
+              name:
+                type: string
+                description: The student's name
+              age:
+                type: integer
+                description: The student's age
+    """
     sql = text("SELECT * FROM students")
     result = db.session.execute(sql)
     
