51446bca7e1c5d45b2b9247a9b05ca78ce4009c0
Vijay <vijay@finflock.com>
2024-11-20 19:58:14 +0530

    route change

--- app.py
+++ app.py
@@ -1,69 +1,3 @@
-# from flask import Flask, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import text
-# from flasgger import Swagger
-# from flask import redirect
-
-# app = Flask(__name__)
-
-# # Replace with your SQL Server connection details
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SA:vijay123@FINFLOCK2\\SQLEXPRESS/student?driver=ODBC+Driver+17+for+SQL+Server'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
-# # Set Swagger UI URL to root `/`
-# app.config['SWAGGER'] = {
-#     'uiversion': 3,
-#     'openapi': '3.0.2'
-# }
-# swagger = Swagger(app)
-
-
-
-# @app.route('/')
-# def index():
-#     return redirect('/apidocs')
-
-
-# @app.route('/students', methods=['GET'])
-# def get_students():
-#     """
-#     Get all students
-#     ---
-#     responses:
-#       200:
-#         description: A list of students
-#         schema:
-#           type: array
-#           items:
-#             type: object
-#             properties:
-#               id:
-#                 type: integer
-#                 description: The student ID
-#               name:
-#                 type: string
-#                 description: The student's name
-#               age:
-#                 type: integer
-#                 description: The student's age
-#     """
-#     sql = text("SELECT * FROM students")
-#     result = db.session.execute(sql)
-    
-#     # Get column names
-#     columns = result.keys()
-    
-#     # Convert each row into a dictionary
-#     students = [dict(zip(columns, row)) for row in result]
-    
-#     return jsonify(students)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)    
-
-
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
@@ -72,8 +6,8 @@ from flask import redirect
 
 app = Flask(__name__)
 
-# Replace with your PostgreSQL connection details
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@172.21.0.3:5432/student'  
+# Replace with your SQL Server connection details
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SA:vijay123@FINFLOCK2\\SQLEXPRESS/student?driver=ODBC+Driver+17+for+SQL+Server'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -85,9 +19,12 @@ app.config['SWAGGER'] = {
 }
 swagger = Swagger(app)
 
-# @app.route('/')
-# def index():
-#     return redirect('/apidocs')
+
+
+@app.route('/')
+def index():
+    return redirect('/apidocs')
+
 
 @app.route('/students', methods=['GET'])
 def get_students():
@@ -124,4 +61,67 @@ def get_students():
     return jsonify(students)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)    
+
+
+# from flask import Flask, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import text
+# from flasgger import Swagger
+# from flask import redirect
+
+# app = Flask(__name__)
+
+# # Replace with your PostgreSQL connection details
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@172.21.0.3:5432/student'  
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# # Set Swagger UI URL to root `/`
+# app.config['SWAGGER'] = {
+#     'uiversion': 3,
+#     'openapi': '3.0.2'
+# }
+# swagger = Swagger(app)
+
+# # @app.route('/')
+# # def index():
+# #     return redirect('/apidocs')
+
+# @app.route('/students', methods=['GET'])
+# def get_students():
+#     """
+#     Get all students
+#     ---
+#     responses:
+#       200:
+#         description: A list of students
+#         schema:
+#           type: array
+#           items:
+#             type: object
+#             properties:
+#               id:
+#                 type: integer
+#                 description: The student ID
+#               name:
+#                 type: string
+#                 description: The student's name
+#               age:
+#                 type: integer
+#                 description: The student's age
+#     """
+#     sql = text("SELECT * FROM students")
+#     result = db.session.execute(sql)
+    
+#     # Get column names
+#     columns = result.keys()
+    
+#     # Convert each row into a dictionary
+#     students = [dict(zip(columns, row)) for row in result]
+    
+#     return jsonify(students)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
