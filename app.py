0b1a8239459805a5cad480e8511373a586923eb9
vijay-prajapati <vijayprajapati260263@gmail.com>
2025-02-24 17:15:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ app = Flask(__name__)
 
 DJANGO_API_URL = "https://prod-quant.vaneck.com/user_data/"
 
-# Create a session object to persist cookies
-session = requests.Session()
-
 @app.route("/get_user_data", methods=["GET"])
 def get_user_data():
-    # Automatically forward all cookies from the Flask request to the Django API
-    response = session.get(DJANGO_API_URL, cookies=request.cookies)
-
+    response = requests.get(DJANGO_API_URL, cookies=request.cookies)
     return jsonify(response.json())
 
 if __name__ == "__main__":
